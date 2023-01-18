253d610792557651405c5008b9111e0e99a3f32c
Jamir Hossain <contact.jamir@gmail.com>
2023-01-18 18:27:14 +0600

    for dev commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.wpdev-planet-icon').forEach(element => {
+    // console.log(element)
+    element.addEventListener('mouseenter', () => {
+        element.style = "animation-play-state: paused;"
+    })
+    element.addEventListener('mouseleave', () => {
+        element.style = "animation-play-state: running;"
+    })
+})
